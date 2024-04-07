0f96dddd543b54f76f7a7f87108f02ca06774e95
Mikhailo Kononenko <kononmi1@fel.cvut.cz>
2024-04-07 19:50:14 +0200

    [Functionality] Add params and getEl by ID

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ const score = document.getElementById('score');
 const highScoreText = document.getElementById('highScore');
 const eatingSound = new Audio('static/sounds/lunch.wav');
 const gridSize = 20;
+let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
+let highScore = 0;
+let direction = 'right';
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStarted = false;
+let isPaused = false;
+
+document.getElementById('up-btn').addEventListener('click', function() { changeDirection('up'); });
+document.getElementById('down-btn').addEventListener('click', function() { changeDirection('down'); });
+document.getElementById('left-btn').addEventListener('click', function() { changeDirection('left'); });
+document.getElementById('right-btn').addEventListener('click', function() { changeDirection('right'); });
+document.getElementById('enter-btn').addEventListener('click', function() {
+  if (!gameStarted) {
+    startGame();
+  }
+});
+document.getElementById('pause-btn').addEventListener('click', pauseGame);
+
 
 
 
