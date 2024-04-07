5184ae6883afcaeb712a8f86a45f3335e9a857ea
Mikhailo Kononenko <kononmi1@fel.cvut.cz>
2024-04-07 19:49:56 +0200

    [Functionality] Add vars to the project

---
+++ script.js
@@ -0,0 +1,41 @@
+//  JavaScript Snake
+const board = document.getElementById('game-board');
+const instructionText = document.getElementById('instruction');
+const logo = document.getElementById('logo');
+const score = document.getElementById('score');
+const highScoreText = document.getElementById('highScore');
+const eatingSound = new Audio('static/sounds/lunch.wav');
+const gridSize = 20;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
